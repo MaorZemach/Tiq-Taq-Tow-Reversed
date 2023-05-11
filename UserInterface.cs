b51dee223f21430790a7c04673af632e5057a713
Lior Roife <72598478+TheSkyDK@users.noreply.github.com>
2023-05-11 14:04:48 +0300

    Update UserInterface.cs

--- UserInterface.cs
+++ UserInterface.cs
@@ -1,55 +1,42 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-
-namespace B23_Ex02_LiorRoife_206257826_MaorZemach_206273690
+namespace Reversed_TicTacToe_For_Console
 {
-   public class UserInterface
-   {
-        private static Game m_CurrentGame;
+    public class UserInterface
+    {
         public static void RunGame()
         {
-            
-           
-            for (int i = 0; i < boardSize; i++)
-            {
-                board.Append(i + 1 + "  ");
-                if (i == (boardSize - 1))
-                {
-                    board.Append("|");
-                }
-            }
+            //Ask for input size
+            //Check Input with the game logics
+            //Ask PVP or PVAI
+            //Print empty board
 
-            Console.WriteLine(board);
-        }
+            //while(not Q , not EndGame)
+            //MakeTurn:
+            //get X,Y choice until it's legal or Q
+            //check validity and print error if Cell is taken
 
-        public static void GameData()
-        {
-            int boardSize = getTheBoardSize();
-            bool isPlayerVersusComputer = checkIfPlayerVresusTheComputer();
+            //clear prev board and print new board
 
-            if(isPlayerVersusComputer == true)
-            {
-                m_CurrentGame = new Game(boardSize,"Player 1","Computer", isPlayerVersusComputer);
-            }
-            else
-            {
-                m_CurrentGame = new Game(boardSize, "Player 1", "Player 2", isPlayerVersusComputer);
-            }
-        }
-        public static void PrintBoard(Game i_CurrGame)
-        {
-            StringBuilder board = new StringBuilder();
+            //Check EndGame?
+            //-no , switch player
+            //-yes (if vertical / diagonal / horizontal win / TIE / Q) print winner & score
+            //ask user if he wants new game (with same score and board size)
+
+
 
         }
-
-        private static int getTheBoardSize()
+        public void GetBoardSize()
         {
-            Console.WriteLine("Please enter the size of the game board(The size must be between 3 to 9):");
+            Console.WriteLine("Please enter the size of the game board (the board is a square shaped and its' size must be between 3 to 9):");
             string sizeOfTheBoard = Console.ReadLine();
             int BoardSize = int.Parse(sizeOfTheBoard);
 
-            while(Game.isTheSizeOfTheBoardValid(BoardSize) == false)
+            while (GameLogics.CheckGameBoardSize(BoardSize) == false)
             {
                 Console.WriteLine("Invalid input. The number is not in the range , please enter new number (The size must be between 3 to 9):");
                 sizeOfTheBoard = Console.ReadLine();
@@ -57,25 +44,5 @@ namespace B23_Ex02_LiorRoife_206257826_MaorZemach_206273690
             BoardSize = int.Parse(sizeOfTheBoard);
             return BoardSize;
         }
-
-        private static bool checkIfPlayerVresusTheComputer()
-        {
-            bool isPlayerAgainstComputer = false;
-            Console.WriteLine("If you want to play against the computer press:'c' , if you want to play against another player press: 'p'");
-            string playerChoice = Console.ReadLine();
-
-            while (playerChoice != "c" && playerChoice!="p")
-            {
-                Console.WriteLine("Invalid input. for game against the computer press:'c', for game against other player press:'p'");
-                playerChoice = Console.ReadLine();
-            }
-
-            if(playerChoice == "c")
-            {
-                isPlayerAgainstComputer = true;
-            }
-
-            return isPlayerAgainstComputer;
-        }    
-   }
+    }
 }
