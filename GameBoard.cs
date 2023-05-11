b7b201a21c1a26bcb0014c244901e2804a1516f7
Lior Roife <72598478+TheSkyDK@users.noreply.github.com>
2023-05-11 14:02:55 +0300

    Update GameBoard.cs

--- GameBoard.cs
+++ GameBoard.cs
@@ -1,33 +1,26 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Ex02.ConsoleUtils;
 
-
-namespace B23_Ex02_LiorRoife_206257826_MaorZemach_206273690
+namespace Reversed_TicTacToe_For_Console
 {
     public class GameBoard
     {
-
         private int m_AmountOfMarkedBoardCells;
         private int m_BoardSize;
         private char[,] m_GameBoard;
 
         public GameBoard(int i_BoardSize)
         {
+            m_AmountOfMarkedBoardCells = 0;
             m_BoardSize = i_BoardSize;
             m_GameBoard = new char[m_BoardSize, m_BoardSize];
-            m_AmountOfMarkedBoardCells = 0;
             gameBoardInitialization();
         }
-        private void gameBoardInitialization()
-        {
-            for (int row = 0; row < m_BoardSize; row++)
-            {
-                for (int col = 0; col < m_BoardSize; col++)
-                {
-                    m_GameBoard[row, col] = ' ';
-                }
-            }
-        }
+
         public int AmountOfMarkedBoardCells
         {
             get
@@ -49,6 +42,17 @@ namespace B23_Ex02_LiorRoife_206257826_MaorZemach_206273690
             set
             {
                 m_BoardSize = value;
+            }
+        }
+
+        private void gameBoardInitialization()
+        {
+            for (int row = 0; row < m_BoardSize; row++)
+            {
+                for (int col = 0; col < m_BoardSize; col++)
+                {
+                    m_GameBoard[row, col] = ' ';
+                }
             }
         }
     }
